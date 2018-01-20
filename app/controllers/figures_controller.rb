42dc133f476033ddb880292e4fa52ef5aafcadc5
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-01-20 18:06:30 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -57,7 +57,6 @@ class FiguresController < ApplicationController
     if !params[:title][:name].empty?
       title_new = Title.create(name: params[:title][:name])
       @figure.titles << title_new
-      #binding.pry
     end
   end
 end
